2efbeff25ae8269f51e95df932c91b7553c8a6c4
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-02-04 15:12:33 +0800

    improved a generating message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 function displayJoke(response) {
   console.log("Joke generated");
+  console.log(response);
 
   new Typewriter("#joke", {
     strings: response.data.answer,
     autoStart: true,
-    Delay: 1,
+    Delay: 2,
     cursor: "",
   });
 }
@@ -16,9 +17,13 @@ function generateDadJoke(event) {
   let apiKey = "5t5a295fb1483fe5b29973001ac3oefd";
 
   let context =
-    "you are a dad and love to tell silly jokes with puns. Your mission is to generate a short joke in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Sign the joke with 'ShesCodes AI' in <strong> element at the end of the poem";
+    "you are a dad and love to tell silly jokes. Your mission is to generate a short joke in basic HTML and separate each line with a <br />. Dont refer to HTML in your response. Make sure to follow the user instructions. Sign the joke with 'ShesCodes AI' in <strong> element at the end of the poem";
   let prompt = `User instructions: Generate a dad joke about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.classList.remove("hidden");
+  jokeElement.innerHTML = ` <div class="generating"> ⌛ Generating a dad joke about ${instructionsInput.value}</div>`;
 
   console.log("generating joke");
   console.log(`prompt ${prompt}`);
