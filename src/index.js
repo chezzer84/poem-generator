e3f37cdfa23dc026799aa39e8ee4d2379937803a
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-02-04 14:16:37 +0800

    added basic form handling and added typewriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateDadJoke(event) {
+  event.preventDefault();
 
+  new Typewriter("#joke", {
+    strings: "Joke will go here about the topic",
+    autoStart: true,
+    Delay: 1,
+    cursor: "",
+  });
+}
+
+let jokeFormElement = document.querySelector("#dad-joke-generator");
+jokeFormElement.addEventListener("submit", generateDadJoke);
